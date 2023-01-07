9dd6e8ec53a293b4a3eb9772f0117348c312ade4
sumitradas94 <sumitradas94@gmail.com>
2023-01-07 08:15:18 +0530

    Method calling for UC1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@
 
 
             Console.WriteLine("Welcome to Address Book Program");
+            Address_Contact obj = new Address_Contact("Ram", "Sharma", "JubliHills,pune", "Pune", "Maharashtra", "90200", "92xxxxxxxx", "ramsharma@gmail.com");
+            // Contact obj1 = new Contact();
+            obj.Add();
+
 
 
         }
