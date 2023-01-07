4b3f8b66efd9ecb27eea1373d7a996f03793db1f
sumitradas94 <sumitradas94@gmail.com>
2023-01-07 08:51:43 +0530

    modified for-UC3

--- Program.cs
+++ Program.cs
@@ -11,8 +11,24 @@
             // Contact obj1 = new Contact();
             // obj.Add();
 
-            AddContact obj = new AddContact();
-            obj.Add();
+            // AddContact obj = new AddContact();
+            // obj.Add();
+
+            EditContact obj = new EditContact();
+            for (int i = 1; i <= 2; i++)
+            {
+                Console.WriteLine("\n Enter Contact Details {0}", i);
+                obj.CreateUser();
+            }
+            obj.ShowAllPersonsInList();
+            Console.WriteLine("\n Do you want to Edit Information: if yes=1, no=2");
+            int A = Convert.ToInt32(Console.ReadLine());
+            if (A == 1)
+            {
+                obj.EditUserInformation();
+                Console.WriteLine("\n Contact Edited");
+                obj.ShowAllPersonsInList();
+            }
 
 
 
