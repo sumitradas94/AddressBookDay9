2336f1efc06fe0fdb05c71330e81d6655e6fca8b
sumitradas94 <sumitradas94@gmail.com>
2023-01-07 08:22:14 +0530

    modified for UC2

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@
         {
 
 
-            Console.WriteLine("Welcome to Address Book Program");
-            Address_Contact obj = new Address_Contact("Ram", "Sharma", "JubliHills,pune", "Pune", "Maharashtra", "90200", "92xxxxxxxx", "ramsharma@gmail.com");
+            // Console.WriteLine("Welcome to Address Book Program");
+            // Address_Contact obj = new Address_Contact("Ram", "Sharma", "JubliHills,pune", "Pune", "Maharashtra", "90200", "92xxxxxxxx", "ramsharma@gmail.com");
             // Contact obj1 = new Contact();
+            // obj.Add();
+
+            AddContact obj = new AddContact();
             obj.Add();
 
 
