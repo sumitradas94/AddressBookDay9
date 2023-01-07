4b661470abc5b9dac4aff99202517a11b304e474
sumitradas94 <sumitradas94@gmail.com>
2023-01-07 08:14:39 +0530

    Create contact

---
+++ Address Contact.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Address_Contact
+    {
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string Zip;
+        public string PhoneNumber;
+        public string Email;
+
+
+        public Address_Contact() { }
+
+
+        public Address_Contact(string FirstName, string LastName, string Address, string City, string State, string Zip, string PhoneNumber, string Email)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.Zip = Zip;
+            this.PhoneNumber = PhoneNumber;
+            this.Email = Email;
+        }
+        public void Add()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " " + Address + " " + City + " " + State + " " + Zip + " " + PhoneNumber + " " + Email);
+
+        }
+    }
+}
